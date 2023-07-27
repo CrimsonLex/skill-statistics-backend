70075ef2e90b33a246abe6a4e58d6d5c1f7409f6
Alexander Samacá Burbano <alexander.samaca@encora.com>
2023-07-27 11:04:31 -0500

    Initial commit

---
+++ src/main/java/com/backend/skillstatisticsbackend/SkillStatisticsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.skillstatisticsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillStatisticsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillStatisticsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/skillstatisticsbackend/SkillStatisticsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.skillstatisticsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillStatisticsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
